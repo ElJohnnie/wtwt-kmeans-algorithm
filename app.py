58c3759d007a04be159347c80d355bfef10357ef
Jonatha <jonatha.follmer@telefonicati.onmicrosoft.com>
2025-03-15 17:46:47 -0300

    fix: change flask host

--- app.py
+++ app.py
@@ -13,7 +13,22 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret')
 def home():
     return f"Environment: {os.getenv('FLASK_ENV', 'unknown')}"
 
+from flask import Flask
+from dotenv import load_dotenv
+import os
+from routes import setup_routes
+
+load_dotenv()
+
+app = Flask(__name__)
+setup_routes(app)
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret')
+
+def home():
+    return f"Environment: {os.getenv('FLASK_ENV', 'unknown')}"
+
 if __name__ == '__main__':
     debug = os.getenv('DEBUG', 'False') == 'True'
     port = int(os.getenv('PORT', 5050))
-    app.run(debug=debug, port=port)
+    app.run(host='0.0.0.0', debug=debug, port=port)
